c0065253413c2f185b5782d4e78f11ce9f539e87
David Sánchez <davidsnhr@gmail.com>
2025-03-05 19:32:16 -0600

    Updated changes

--- src/core/game.py
+++ src/core/game.py
@@ -2,6 +2,9 @@ import pygame
 import random
 from core.config import Config
 from entities.player import Player
+from builder.director import EnemyDirector
+from builder.enemy_builder import EnemyBuilder
+from src.utils import check_collision
 
 pygame.init()
 
@@ -14,4 +17,61 @@ pygame.display.set_caption("Space Invader")
 #Jugador
 player = Player(WIDTH//2, HEIGHT -80)
 
-#
+#Enemies
+director = EnemyDirector()
+builder = EnemyBuilder()
+
+enemies = [
+    director.construct_enemy(builder, "normal"),
+    director.construct_enemy(builder, "fast"),
+    director.construct_enemy(builder, "strong")
+]
+
+bullets = []
+
+enemy_speed = 1
+enemy_spawn_timer = 0
+
+running = True
+while running:
+    pygame.time.delay(30)
+    screen.fill((0, 0, 0))
+    
+    
+    #Eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    #Controles
+    keys = pygame.key.get_pressed()
+    player.move(keys, WIDTH)
+    
+    if keys[pygame.K_SPACE]:
+        bullets.append(player.shoot())
+    
+    
+    for bullet in bullets[:]:
+        bullet.move()
+        if bullet.y < 0:
+            bullet.remove(bullet)
+    
+    for enemy in enemies[:]:
+        enemy.move()
+        if check_collision(enemy, player):
+            running = False
+    
+    for bullet in bullets[:]:
+        for enemy in enemies[:]:
+            if check_collision(bullet, enemy):
+                enemies.remove(enemy)
+                bullets.remove(bullet)
+                break
+    
+    
+
+    
+    
+        
+     
+    
